a270c680eab9c1c1d506a5b978735206212efdda
Asako Kondo <kondoasako@gmail.com>
2021-07-08 22:49:13 +0900

    HTMLを生成する

---
+++ 2/data.js
@@ -0,0 +1,13 @@
+window.messages =
+[
+  {
+    date: "2015-09-01",
+    type: "error",
+    text: "エラーが発生しました"
+  },
+  {
+    date: "2015-09-01",
+    type: "warning",
+    text: "バッテリーが少なくなってます"
+  }
+]
