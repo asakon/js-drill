bf33b01f6424bb51abb686a62544d2594b174d64
Asako Kondo <kondoasako@gmail.com>
2021-07-08 21:47:11 +0900

    値が存在するか
    アロー関数で

--- js/some.js
+++ js/some.js
@@ -5,8 +5,10 @@
 
 // some() は、与えられた callback 関数を、配列に含まれる各要素に対して一度ずつ、callback が真と解釈される値 (論理型に変換した際に真となる値) を返す要素が見つかるまで呼び出します。そのような要素が見つかると、some() はただちに true を返します
 
-function isBiggerThan10(element, index, array) {
-	return element > 10;
+const fruits = ['banana', 'apple', 'grape', 'watermelon'];
+
+function checkAvailability(arr, val) {
+	return arr.some(arrVal => val === arrVal);
 }
 
-console.log([2, 5, 11, 1, 4].some(isBiggerThan10));
+console.log(checkAvailability(fruits, 'apple'));
